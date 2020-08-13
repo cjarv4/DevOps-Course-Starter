488e2d4288e43c9c54a9d733252125f5882b5f75
Carl Jarvis <carl.jarvis1@aexp.com>
2020-08-13 17:41:52 +0100

    clean up code, remove unused methods

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for
-from operator import itemgetter
 import session_items as session
 
 app = Flask(__name__)
@@ -10,11 +9,6 @@ sortSwitch = True
 @app.route('/', methods=['POST', 'GET'])
 def index():
     todo, doing, done = session.get_items()
-    # global sortSwitch
-    # if sortSwitch:
-    #     todo = sorted(todo, key=itemgetter('closed'))
-    # else:
-    #     todo = sorted(todo, key=itemgetter('closed'), reverse=True)
     return render_template('index.html', todos=todo, doings=doing, dones=done)
 
 
@@ -51,16 +45,6 @@ def delete_checklist_item(id, checklistId):
 @app.route('/item/<id>')
 def get_item(id):
     return render_template('todoSingle.html', items=session.get_item(id), checklist=session.get_card_checklist(id))
-
-
-# @app.route('/sorted_items')
-# def sort_item():
-#     global sortSwitch
-#     if sortSwitch:
-#         sortSwitch = False
-#     else:
-#         sortSwitch = True
-#     return redirect("/")
 
 
 if __name__ == '__main__':
--- session_items.py
+++ session_items.py
@@ -7,17 +7,13 @@ debug = False
 
 
 def get_items():
-    """
-    Fetches all saved items from the session.
-
-    Returns:
-        list: The list of saved items.
-    """
     cards, doing, done = get_cards()
 
     if debug:
         print("cards - ")
         print(cards)
+        print(doing)
+        print(done)
         print("boards - ")
         get_boards()
         print("lists - ")
@@ -26,84 +22,29 @@ def get_items():
 
 
 def get_item(id):
-    """
-    Fetches the saved item with the specified ID.
-
-    Args:
-        id: The ID of the item.
-
-    Returns:
-        item: The saved item, or None if no items match the specified ID.
-    """
     items, doing, done = get_items()
     items = items + doing + done
     return next((item for item in items if item['id'] == id), None)
 
 
 def add_item(title):
-    """
-    Adds a new item with the specified title to the session.
-
-    Args:
-        title: The title of the item.
-
-    Returns:
-        item: The saved item.
-    """
-    # items = get_items()
-
-    # Determine the ID for the item based on that of the previously added item
-    # item = create_new_item(items[-1]['id'] + 1 if items else 0, title)
-
     post_trello("https://api.trello.com/1/cards?idList=5f3169df33611522761de7cc&name=" + title)
-    # Add the item to the list
-    # items.append(item)
-    # session['items'] = items
-
-    # return item
-
-
-# https://api.trello.com/1/lists/5f3169df33611522761de7cc/cards?token=0ee7c15267a7a7fee3f90ebed124aa5e14652888ce7fc40b444ab7b9ebcbd019&key=d3cfc4a193b13c194d0ca2484808d1f2
-
-def save_item(item):
-    """
-    Updates an existing item in the session. If no existing item matches the ID of the specified item, nothing is saved.
-
-    Args:
-        item: The item to save.
-    """
-    existing_items = get_items()
-    updated_items = [item if item['id'] == existing_item['id'] else existing_item for existing_item in existing_items]
-
-    session['items'] = updated_items
-
-    return item
-
-
-def delete_item(id):
-    # existing_items = get_items()
-    # existing_items.remove(item)
-    # updated_items = existing_items
-    #
-    # session['items'] = updated_items
-    delete_trello("https://api.trello.com/1/cards/" + id + "?")
 
 
 def complete_item(id):
     put_trello("https://api.trello.com/1/cards/" + id + "/?idList=5f3169dff5e94e5d22ec1d0f")
 
 
-def complete_checklist_item(id,checklist_id):
-    put_trello("https://api.trello.com/1/cards/" + id + "/checkItem/"+checklist_id+"/?state=complete")
+def complete_checklist_item(id, checklist_id):
+    put_trello("https://api.trello.com/1/cards/" + id + "/checkItem/" + checklist_id + "/?state=complete")
 
 
-def delete_checklist_item(id,checklist_id):
-    delete_trello("https://api.trello.com/1/cards/" + id + "/checkItem/"+checklist_id+"/?")
+def delete_item(id):
+    delete_trello("https://api.trello.com/1/cards/" + id + "?")
 
 
-def create_new_item(id, title, status='Not Started'):
-    item = {'id': id, 'title': title, 'status': status}
-    return item
+def delete_checklist_item(id, checklist_id):
+    delete_trello("https://api.trello.com/1/cards/" + id + "/checkItem/" + checklist_id + "/?")
 
 
 def get_boards():
@@ -126,10 +67,6 @@ def get_cards():
 
 def get_card_checklist(id):
     todo = get_trello("https://api.trello.com/1/cards/" + id + "/checklists?")
-    # doing = get_trello("https://api.trello.com/1/lists/5f3169e0916e3156fd3d1680/cards?fields=id,closed,name")
-    # done = get_trellxo("https://api.trello.com/1/lists/5f3169dff5e94e5d22ec1d0f/cards?fields=id,closed,name")
-    # print("printing todo")
-    print(todo)
     if todo:
         return todo[0]["checkItems"]
     else:
@@ -151,8 +88,6 @@ def post_trello(url):
 def put_trello(url):
     url = add_trello_token_and_key(url)
     response = requests.put(url=url)
-    print(url)
-    print(response)
     return response
 
 
