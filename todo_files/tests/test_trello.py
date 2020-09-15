1c6dfd9fb72075b23b7f0a394e7244583ff5f03c
Carl Jarvis <carl.jarvis1@aexp.com>
2020-09-15 13:58:46 +0100

    add test to check if recieving and storing last activity properly

--- todo_files/tests/test_trello.py
+++ todo_files/tests/test_trello.py
@@ -2,9 +2,14 @@ import pytest
 import trello as trello
 import Card as card
 
-def test_first():
-    print("testing")
-    assert 1==1
+# Get a stub card
+
+def create_test_card(title):
+    card.add_card(title=title, desc="made for testing", dueDate="")
+
+def cleanup_test_card(title):
+    test_card = card.get_card_by_name(title)
+    card.delete_card(test_card.id)
 
 def test_trello_connection():
     response = trello.get_trello_board_id()
@@ -15,3 +20,12 @@ def test_get_individual_lists():
     assert isinstance(todo, list)
     assert isinstance(doing, list)
     assert isinstance(done, list)
+
+def test_get_card_last_modified_date():
+    test_card_name = "test_get_card_last_modified_date"
+    try:
+        create_test_card(test_card_name)
+        test_card = card.get_card_by_name(test_card_name)
+        assert test_card.last_activity
+    finally:
+        cleanup_test_card(test_card_name)
