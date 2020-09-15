35d0b3f48f315980b37efd9b5838127b5fee8586
Carl Jarvis <carl.jarvis1@aexp.com>
2020-09-15 10:42:48 +0100

    WIP add first tests to project in test_trello

--- test_trello.py
+++ test_trello.py
@@ -1,5 +1,17 @@
 import pytest
 import trello as trello
+import Card as card
 
 def test_first():
+    print("testing")
     assert 1==1
+
+def test_trello_connection():
+    response = trello.get_trello_board_id()
+    assert response=='5f3169dff2ad7b72d45fc4c3'
+
+def test_get_individual_lists():
+    todo, doing, done = card.get_cards()
+    assert isinstance(todo, list)
+    assert isinstance(doing, list)
+    assert isinstance(done, list)
