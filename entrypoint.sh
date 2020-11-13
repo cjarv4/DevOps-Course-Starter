a00f8308a2fdc74b06408b0745dcb9d3fb7a8432
Carl Jarvis <carl.jarvis1@aexp.com>
2020-11-13 17:09:37 +0000

    set up gunicorn for prod server

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
-cd todo_files
-poetry run flask run --host 0.0.0.0
+#!/bin/sh
+
+if [ "$FLASK_ENV" = "production" ]
+then
+    echo "Creating the production environment..."
+    cd todo_files
+    poetry run gunicorn --bind 0.0.0.0:5000 "app:create_app()"
+fi
+
+if [ "$FLASK_ENV" = "development" ]
+then
+    echo "Creating the develop environment..."
+    cd todo_files
+    poetry run flask run --host 0.0.0.0
+fi
+
+
+exec "$@"
